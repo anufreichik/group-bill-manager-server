de4a90385791281c639e5bbf21a4e69fcfb67d3c
anufreichik <marta149@hotmail.com>
2021-03-24 20:33:43 -0700

    db modified to use env var

--- src/modules/core/db.js
+++ src/modules/core/db.js
@@ -1,16 +1,23 @@
 import mongoose from 'mongoose';
 
+export const connectionString = process.env.MONGO_CONNECTION_STRING;
+
 function dbConnect() {
-  mongoose.connect('mongodb://localhost/GroupBillManager', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  });
+    mongoose.connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    })
+        .catch((err) => console.log(err));
 }
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-  console.log('CONNECTED');
+    console.log('CONNECTED');
+});
+db.on('disconnected', () => {
+    console.log('Mongoose default connection disconnected');
 });
 export default dbConnect;
